04d7b2d7e4e65649489f36f082a9cc641eb1cdc9
haito <example@example.com>
2024-10-21 17:17:28 +0300

    Add schemas and hotels.py

--- main.py
+++ main.py
@@ -1,83 +1,12 @@
-from fastapi import Body, FastAPI, Query
-from fastapi.openapi.docs import (
-    get_swagger_ui_html,
-    get_swagger_ui_oauth2_redirect_html,
-)
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
-app = FastAPI()
+from hotels import router as router_hotels
 
+app = FastAPI(docs_url=None)
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-]
-
-
-@app.get("/hotels")
-def get_hotels(
-        id: int | None = Query(None),
-        title: str | None = Query(None, description="Название отеля"),
-):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    return hotels_
-
-
-@app.post("/hotels")
-def create_hotel(
-    title: str = Body(embed=True)
-):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": title
-    })
-    return {"status": "OK"}
-
-
-@app.put("/hotels/{hotel_id}")
-def update_hotel(
-    hotel_id: int,
-    title: str = Body(),
-    name: str = Body()
-):
-    global hotels
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["title"] = title
-            hotel["name"] = name
-            return hotel
-    return {"status": "ID Not Found"}
-
-
-@app.patch("/hotels/{hotel_id}")
-def patch_hotel(
-    hotel_id: int,
-    title: str | None = Body(None),
-    name: str | None = Body(None)
-):
-    global hotels
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            if title:
-                hotel["title"] = title
-            if name:
-                hotel["name"] = name
-            return hotel
-    return {"status": "ID Not Found"}
-
-
-@app.delete("/hotels/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
-    return {"status": "OK"}
+app.include_router(router_hotels)
 
 
 @app.get("/docs", include_in_schema=False)
@@ -89,11 +18,6 @@ async def custom_swagger_ui_html():
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
     )
-
-
-@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-async def swagger_ui_redirect():
-    return get_swagger_ui_oauth2_redirect_html()
 
 
 if __name__ == "__main__":

a3023d260a0c02a4312ca9eed8781cb6e5081bf8
haito <example@example.com>
2024-11-10 01:10:45 +0300

    update post handle for db

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,9 @@
 from fastapi import Body, Query, APIRouter
+from sqlalchemy import insert
 
+from models.hotels import HotelsOrm
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
 from src.schemas.hotels import Hotel, HotelPatch
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -38,23 +41,21 @@ def get_hotels(
 
 
 @router.post("")
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {"summary": "Сочи", 'value': {
         "title": "Hotel Sochi 5 stars u morya",
-        "name": "sochi_u_morya",
+        "location": "ul. Morya, 1",
     }},
     "2": {"summary": "Dubai", 'value': {
         "title": "Hotel Dubai fountain",
-        "name": "dubai_fountain",
+        "location": "ul. Sheikha, 2",
     }},
     })
 ):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": hotel_data.title,
-        "name": hotel_data.name,
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
     return {"status": "OK"}
 
 
